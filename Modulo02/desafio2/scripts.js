ca02b7c917f8bb4e5c6dafb3aa2088b8f58c06c0
Ian Prioste <ian.prioste@gmail.com>
2020-07-01 22:04:51 -0300

    Desafio

--- Modulo02/desafio2/scripts.js
+++ Modulo02/desafio2/scripts.js
@@ -3,14 +3,14 @@ const cards = document.querySelectorAll('.card')
 
 for (let card of cards){
     card.addEventListener("click", function(){
-        const videoId = card.getAttribute('id')
+        let videoId = card.getAttribute('id')
         modalOverlay.classList.add('active')
-        modalOverlay.querySelector('iframe').src =`https://www.youtube.com/embed/${videoId}`
+        modalOverlay.querySelector('iframe').src=`https://www.youtube.com/embed/${videoId}`
     })
 }
 
-document.querySelector('.close-modal').addEventListener('click', function(){
+document.querySelector('.material-icons').addEventListener('click', function(){
     modalOverlay.classList.remove('active')
     modalOverlay.querySelector('iframe').src =""
-})
 
+})
