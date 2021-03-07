16f2d07411ead0dca7e7fe3f6427e5542a88f9a0
Ian Prioste <47223654+ianprioste@users.noreply.github.com>
2021-03-07 18:21:29 -0300

    Update desafio1-2.js

--- Modulo01/desafio1-2.js
+++ Modulo01/desafio1-2.js
@@ -1,72 +1,49 @@
-// Desafios para fortalecer alguns conceitos, entre eles:
+// Desafio para fortalecer conceitos: Objetos e Vetores
 
-// Objetos;
-// Vetores.
-// Construção e impressão de objetos
-// Crie um programa que armazena dados da empresa Rocketseat dentro de um objeto chamado empresa. Os dados a serem armazenados são:
+/* Construção e impressão de Objetos
 
-// Nome: Rocketseat
+Crie um programa que armazena dados da empresa Rocketseat dentro de um objeto chamado empresa. Os dados a serem armazenados são:
+- Nome: Rocketseat
+- Cor: Roxo
+- Foco: Programação
+- Endereço:
+- Rua: Rua Guilherme Gembala
+- Número: 260
+Obs.: Para armazenar os dados de endereço da empresa você pode criar objetos intercalados*/ 
 
-// Cor: Roxo
-
-// Foco: Programação
-
-// Endereço:
-
-// Rua: Rua Guilherme Gembala
-// Número: 260
-// Obs.: Para armazenar os dados de endereço da empresa você pode criar objetos intercalados, por exemplo:
-
-// const usuario = {
-//     nome: "Diego",
-//     empresa: {
-//       nome: "Rocketseat"
-//     }
-//   };
-
-const empresas = [
-    {
-        nome: "Rocketseat",
-        cor: "Roxo",
-        foco: "Programação",
-        endereco: {
-            rua: "Rua Guilherme Gembala",
-            numero: 260
-        }
+const empresa = {
+    nome: "Rocketseat",
+    cor: "Roxo",
+    foco: "Programação",
+    endereco:{
+        rua:"Rua Guilherme Gembala",
+        numero:260,
     }
-] 
+}
 
 //Imprima em tela utilizando console.log o nome da empresa e seu endereço no seguinte formato:
 
+console.log (`A empresa ${empresa.nome} está localizada em ${empresa.endereco.rua}, ${empresa.endereco.numero}`)
 
-console.log(`A empresa ${empresas[0].nome} está localizada em ${empresas[0].endereco.rua}, ${empresas[0].endereco.numero}`)
+/* Vetores e objetos
 
+Crie um programa com um objeto para armazenar dados de um programador como nome, idade e tecnologias que trabalha.
+Um programador pode trabalhar com várias tecnologias, por isso armazene essas tecnologias em um array.
+As tecnologias também devem ser objetos contendo nome e especialidade. */
 
-// Vetores e objetos
-// Crie um programa com um objeto para armazenar dados de um programador como nome, idade e tecnologias que trabalha.
-
-// Um programador pode trabalhar com várias tecnologias, por isso armazene essas tecnologias em um array.
-
-// As tecnologias também devem ser objetos contendo nome e especialidade, use as tecnologias abaixo:
-
-// { nome: 'C++', especialidade: 'Desktop' }
-// { nome: 'Python', especialidade: 'Data Science' }
-// { nome: 'JavaScript', especialidade: 'Web/Mobile' }
-
-const programadores = [{
-    nome: 'Carlos',
-    idade: 32,
-    especialidades: [{
-        tecnologia: "C++",
-        especialidade:"Desktop"
-    },
-    {
-        tecnologia: "JavaScript", 
-        especialidade: "Web/Mobile"
-    }
+const programador = {
+    nome: "Ian",
+    idade: 28,
+    tecnologias: [
+        {
+            nome: "JavaScript",
+            especialidade: "Web/Mobile"
+        },
+        {
+            nome: "HTML5",
+            especialidade: "Web/Mobile"
+        }
     ]
 }
-]
 
-console.log(`O usuário ${programadores[0].nome} tem ${programadores[0].idade} e usa a tecnologia ${programadores[0].especialidades[0].tecnologia} com especialidade em ${programadores[0].especialidades[0].especialidade}`)
-
+console.log(`O usuário ${programador.nome} tem ${programador.idade} anos e usa a tecnologia ${programador.tecnologias[0].nome} com especialidade em ${programador.tecnologias[0].especialidade}`)
