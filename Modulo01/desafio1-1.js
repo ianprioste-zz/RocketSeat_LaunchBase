5b90fcda12129f02cb422b1484c9c9a74341eb48
Ian Prioste <47223654+ianprioste@users.noreply.github.com>
2021-03-07 17:39:26 -0300

    Update desafio1-1.js

--- Modulo01/desafio1-1.js
+++ Modulo01/desafio1-1.js
@@ -1,55 +1,41 @@
-// Desafios para fortalecer alguns conceitos, entre eles:
+/* Desafio para fortalecer conceitos de: Variáveis; Condicionais; Operadores */
 
-// Variáveis;
-// Condicionais;
-// Operadores.
+//Cálculo IMC
+//Crie um programa para calcular o IMC e nível de obesidade de uma pessoa.
 
-// Crie um programa para calcular o IMC e nível de obesidade de uma pessoa.
+const nome = "Ian";
+const peso = 77;
+const altura = 1.57;
 
-// Comece criando constantes para armazenar o nome, peso, altura e sexo de uma pessoa, por exemplo:
+const imc = peso/(altura*altura);
 
-// const nome = "Carlos";
-// const peso = 200;
-// const altura = 1.88;
+if (imc >=30){
+    console.log(`${nome} você está acima do peso. Seu IMC é ${imc}`)
+} else{
+    console.log(`${nome} você não está acima do peso. Seu IMC é ${imc}`)
+}
 
-// const imc = peso/(altura*altura);
 
-// // Baseado no valor obtido através desse cálculo exiba as seguintes mensagens:
-
-// // SE o IMC maior ou igual a 30: Carlos você está acima do peso;
-// // SE o IMC menor que 29.9: Carlos você não está acima do peso;
-
-// if (imc >= 30){
-//     console.log(`${nome} você está acima do peso, mas está tudo bem! Acompanhe sua saúde com um médico e um bom nutricionita. IMC sozinho não quer dizer nada :)`)
-// } else {
-//     console.log(`${nome}, você não está acima do peso. Mas isso não quer dizer nada! Acompanhe sua saúde com um médico e um bom nutricionista.`)
-// }
-
+const nome = "Ian"
+const sexo = "M"
+const idade = 28
+const contribuicao = 10
 
 // Cálculo de aposentadoria
-
 // Crie um programa para calcular a aposentadoria de uma pessoa.
 
-// Obs.: Esse cálculo é fictício, dentro da aposentadoria existem muitos outros fatores para serem levados em conta :)
-
-// Comece criando constantes para armazenar nome, sexo, idade e contribuicao(em anos), por exemplo:
-
-const nome = "Silvana";
-const sexo = "F";
-const idade = 42;
-const contribuicao = 50;
-
-// Baseado nos valores acima utilize as fórmulas a seguir para calcular se a pessoa está apta ou não para se aposentar e no fim imprima uma mensagem em tela.
-
-// O tempo de contribuição mínimo para homens é de 35 anos e, para mulheres, 30 anos;
+//O tempo de contribuição mínimo para homens é de 35 anos e, para mulheres, 30 anos;
 // Utilizando a regra 85-95, a soma da idade com o tempo de contribuição do homem precisa ser de no mínimo 95 anos, enquanto a mulher precisa ter no mínimo 85 anos na soma;
-// Com base nas regras acima imprima na tela as mensagens:
 
-// SE a pessoa estiver aposentada: Silvana, você pode se aposentar!;
-// SE a pessoa NÃO estiver aposentada: Silvana, você ainda não pode se aposentar!;
+const minimo = idade + contribuicao
 
-if(sexo=='F' & idade+contribuicao>= 85 | sexo=='M' & idade+contribuicao>=95){
-    console.log(`${nome}, você pode se aposentar!`, idade+contribuicao)
+const homemPodeAposentar = sexo =='M' && contribuicao >=35 && minimo >= 95
+const mulherPodeAposentar = sexo =='F' && contribuicao >=30 && minimo >= 85
+
+if (homemPodeAposentar||mulherPodeAposentar){
+    console.log(`${nome} você pode se aposentar`)
 } else {
-        console.log(`${nome}, você ainda não pode se aposentar!`, idade+contribuicao)
+    console.log(`${nome} você não pode se aposentar`)
 }
+
+
