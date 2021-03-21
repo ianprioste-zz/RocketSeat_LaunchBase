7cafee4e5fb8535829ad8dce85898646c51acada
Ian Prioste <ian.prioste@gmail.com>
2021-03-21 20:43:55 -0300

    desafio3.2

---
+++ Desafios/Desafios 3/data.js
@@ -0,0 +1,21 @@
+module.exports = [
+    {
+        id:"gyGsPlt06bo",
+        title: "Web Design Trends 2021",
+        channel: "Envato",
+        time: "13:36"
+    },
+    {
+        id:"IbOyBIS57C0",
+        title: "Professional Web Design Process Explained in 5 minutes",
+        channel: "Flux",
+        time: "5:54"
+    },
+    {
+        id:"iF66er2oHBo",
+        title: "Como começar a trabalhar com web design",
+        channel: "Clube do Design",
+        time: "6:48"
+    }
+
+]
--- Desafios/Desafios 3/server.js
+++ Desafios/Desafios 3/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
 
+const videos = require("./data")
+
 const server = express()
 
 server.use(express.static('public')) //css na pasta public
@@ -14,15 +16,31 @@ nunjucks.configure("views", {
 })
 
 server.get("/", function(req,res){
-    return res.render("index")
+    const data = {
+        title: "Introduce Your Product Quickly & Effectively",
+        text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.'
+    }    
+
+    return res.render("index",{data})
 })
 
 server.get("/about", function(req,res){
-    return res.render("about")
+    const data = {
+        title: "About our Services Provider",
+        text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.',
+        list:[
+            {title:"Item 1"},
+            {title:"Item 2"},
+            {title:"Item 3"},
+            {title:"Item 4"}
+        ]
+    }    
+
+    return res.render("about",{data})
 })
 
 server.get("/contents", function(req,res){
-    return res.render("contents")
+    return res.render("contents",{items:videos})
 })
 
 server.use(function(req, res) {
