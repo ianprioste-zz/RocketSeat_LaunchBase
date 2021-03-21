c225336f06dc1ee6b3903dbece21963f79cba1fb
Ian Prioste <ian.prioste@gmail.com>
2021-03-21 16:38:14 -0300

    portfolio

---
+++ MODULO 03/data.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+        id:"jU9br5C-wqI",
+        title: "The Difference Between Digitization, Digitalization, and Digital Transformation",
+        duration: "24 min",
+        price:"Free"
+    },
+    {
+        id:"KvZRRIRcbHY",
+        title: "Digital Transformation Vision 2020",
+        duration: "24 min",
+        price:"Free"
+    },
+    {
+        id:"HRvkbpbUM2M",
+        title: "Pessoa, tecnologia e negócios: os 3 pilares da Transformação Digital no Marketing",
+        duration: "24 min",
+        price:"Free"
+    }
+]
--- MODULO 03/server.js
+++ MODULO 03/server.js
@@ -2,21 +2,37 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const videos = require('./data') // ./ (ponto barra) faz referencia a raiz do projeto
+
 
 server.use(express.static('public'))
 
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoscape: false
 })
 
 server.get("/", function(req,res){
-    return res.render("about")
+    const data = {
+        avatar_url:"https://avatars.githubusercontent.com/u/47223654?s=400&u=4869f854557563262fa63fd8da621d1a9ce7e92e&v=4",
+        name:"Ian Prioste",
+        role:"Scrum Master | Analista de Trasnformação Digital",
+        description:"Analista de Transformação Digital. Graduado em Sistemas de Informação. Responsável por acelerar a Transformação Digital na área de Regulatory Science de uma empresa multinacional, promovendo a mudança de mindset através da facilitação de workshops de inovação, exploração e viabilização de novas tecnologias, otimização de processos e implementação de metodologias ágeis.",
+        links:[
+            { name: "Github", url:"https://github.com/ianprioste" },
+            { name: "LinkedIn", url:"https://www.linkedin.com/in/ianprioste/"}
+        ]
+    }
+
+    return res.render("about",{data})
 })
 
 server.get("/portfolio", function(req,res){
-    return res.render("portfolio")
+
+    return res.render("portfolio",{items: videos})
+
 })
 
 server.listen(5000, function(){
